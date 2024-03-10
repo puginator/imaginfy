1102fbf9f2d1598a65720a5f7da14345a850043c
Austin Amento <76490816+puginator@users.noreply.github.com>
2024-03-10 14:12:02 -0700

    Fix key issue in Card component and update Image model

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -45,7 +45,7 @@ export const Collection = ({
 
     router.push(newUrl, {scroll: false});
   };
-
+  console.log(images)
   return (
     <>
       <div className="collection-heading">
@@ -56,7 +56,7 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image.author._id} />
+            <Card image={image} key={image._id} />
           ))}
         </ul>
       ) : (
